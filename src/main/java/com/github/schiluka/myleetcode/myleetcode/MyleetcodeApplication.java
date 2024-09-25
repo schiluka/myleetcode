278dfe204fceacc9df8acb67f87d04331d460c61
schiluka <san.chiluka@gmail.com>
2024-09-24 23:03:12 -0400

    main file add

---
+++ src/main/java/com/github/schiluka/myleetcode/myleetcode/MyleetcodeApplication.java
@@ -0,0 +1,13 @@
+package com.github.schiluka.myleetcode.myleetcode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyleetcodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyleetcodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/schiluka/myleetcode/myleetcode/MyleetcodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.schiluka.myleetcode.myleetcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyleetcodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
